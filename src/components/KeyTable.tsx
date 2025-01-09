b82267cca41ee696c918c811d7239e78ffe9db74
hotaryuzaki <hotaru_edogawa@yahoo.com>
2025-01-09 22:41:10 +0700

    UPDATE bug fixed: 1. USERS 2. KEYS 3. COPIES

--- src/components/KeyTable.tsx
+++ src/components/KeyTable.tsx
@@ -13,32 +13,44 @@ const KeyTable = ({ columns, data }: any) => {
   return (
     <table {...getTableProps()} className="min-w-full divide-y divide-gray-200">
       <thead className="bg-gray-50">
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th
-                {...column.getHeaderProps()}
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                {column.render('Header')}
-              </th>
-            ))}
-          </tr>
-        ))}
+        {headerGroups.map(headerGroup => {
+          const { key, ...restHeaderProps } = headerGroup.getHeaderGroupProps();
+          return (
+            <tr {...restHeaderProps} key={key}>
+              {headerGroup.headers.map(column => {
+                const { key, ...restColumnProps } = column.getHeaderProps();
+                return (
+                  <th
+                    {...restColumnProps}
+                    key={key}
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    {column.render('Header')}
+                  </th>
+                );
+              })}
+            </tr>
+          );
+        })}
       </thead>
       <tbody {...getTableBodyProps()} className="bg-white divide-y divide-gray-200">
         {rows.map(row => {
           prepareRow(row);
+          const { key, ...restRowProps } = row.getRowProps();
           return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => (
-                <td
-                  {...cell.getCellProps()}
-                  className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
-                >
-                  {cell.render('Cell')}
-                </td>
-              ))}
+            <tr {...restRowProps} key={key}>
+              {row.cells.map(cell => {
+                const { key, ...restCellProps } = cell.getCellProps();
+                return (
+                  <td
+                    {...restCellProps}
+                    key={key}
+                    className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
+                  >
+                    {cell.render('Cell')}
+                  </td>
+                );
+              })}
             </tr>
           );
         })}
